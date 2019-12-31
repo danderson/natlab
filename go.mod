e8a3ec0717331915698dc845004f99147c7f40f1
David Anderson <dave@natulte.net>
2019-12-31 12:02:06 -0800

    Add a CLI wrapper, we'll want multiple commands at some point.

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.13
 require (
 	github.com/florianl/go-nfqueue v1.1.1-0.20191218192739-56eb21f5b2a9
 	github.com/sirupsen/logrus v1.4.2
+	github.com/urfave/cli/v2 v2.1.1
 )
--- main.go
+++ main.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"context"
 	"flag"
-	"net"
-	"time"
+	"os"
 
-	nfqueue "github.com/florianl/go-nfqueue"
-
-	log "github.com/sirupsen/logrus"
+	"github.com/urfave/cli/v2"
 )
 
 var (
@@ -17,84 +13,30 @@ var (
 )
 
 func main() {
-	log.Info("Starting")
-
-	config := nfqueue.Config{
-		NfQueue:      42,
-		MaxPacketLen: 65535,
-		MaxQueueLen:  255,
-		Copymode:     nfqueue.NfQnlCopyPacket,
-		ReadTimeout:  10 * time.Millisecond,
-		WriteTimeout: 15 * time.Millisecond,
+	app := &cli.App{
+		Name:  "natlab",
+		Usage: "Testbed for NAT traversal software",
+		Commands: []*cli.Command{
+			{
+				Name:  "nat",
+				Usage: "hook into kernel datapath and operate as a NAT box",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:     "lan-interface",
+						Aliases:  []string{"L"},
+						Required: true,
+						Usage:    "name of the LAN-side network interface",
+					},
+					&cli.StringFlag{
+						Name:     "wan-interface",
+						Aliases:  []string{"W"},
+						Required: true,
+						Usage:    "name of the WAN-side network interface",
+					},
+				},
+				Action: nat,
+			},
+		},
 	}
-
-	queue, err := nfqueue.Open(&config)
-	if err != nil {
-		log.Fatalf("Connecting to NFQUEUE: %s", err)
-	}
-	defer queue.Close()
-
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	iface, err := net.InterfaceByName(*wanIf)
-	if err != nil {
-		log.Fatalf("Getting %s interface info: %s", *wanIf, err)
-	}
-
-	var publicAddr net.IP
-	addrs, err := iface.Addrs()
-	if err != nil {
-		log.Fatalf("Getting %s interface addrs: %s", *wanIf, err)
-	}
-	for _, addr := range addrs {
-		ipaddr := addr.(*net.IPAddr)
-		if ipaddr == nil || ipaddr.IP.To4() == nil {
-			continue
-		}
-		publicAddr = ipaddr.IP
-		break
-	}
-
-	conntrack := NewAddressAndPortDependentNAT(publicAddr)
-
-	process := func(a nfqueue.Attribute) int {
-		pkt := NewPacket(*a.Payload)
-		if pkt == nil {
-			// We don't know how to handle this kind of packet
-			queue.SetVerdict(*a.PacketID, nfqueue.NfDrop)
-			return 0
-		}
-		intf, err := net.InterfaceByIndex(int(*a.InDev))
-		if err != nil {
-			panic(err)
-		}
-
-		verdict := VerdictDrop
-		switch intf.Name {
-		case *lanIf:
-			verdict = conntrack.MangleOutbound(pkt)
-		case *wanIf:
-			verdict = conntrack.MangleInbound(pkt)
-		}
-
-		switch verdict {
-		case VerdictAccept:
-			queue.SetVerdict(*a.PacketID, nfqueue.NfAccept)
-		case VerdictDrop:
-			queue.SetVerdict(*a.PacketID, nfqueue.NfDrop)
-		case VerdictMangle:
-			queue.SetVerdictModPacket(*a.PacketID, nfqueue.NfAccept, *a.Payload)
-		}
-
-		return 0
-	}
-	err = queue.Register(ctx, process)
-	if err != nil {
-		log.Fatalf("Couldn't register packet processor: %s", err)
-	}
-
-	log.Info("Created tuns")
-	<-ctx.Done()
-	log.Info("Exiting")
+	app.Run(os.Args)
 }
